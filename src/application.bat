92975b8ab6400f24770e01dce2132fcdded4144d
Louis Abid <louis.abid.53@gmail.com>
2015-03-20 16:03:47 +0100

    add var to script

--- src/application.bat
+++ src/application.bat
@@ -1,4 +1,6 @@
 ### application.bat - 2015 - Louis ABID ###
-echo "Copyright company - 201x"
-echo "Build : 10:00"
-echo "version 1.1"
+version = "2"
+build_hour = "11H00"
+
+echo "Build ${build_hour}"
+echo "version ${version}"
